931d365f26d49e7b167de00605956d844d70bbe2
geisbsuj <34568117+geisbsuj@users.noreply.github.com>
2018-01-14 19:16:12 +0100

    Add existing file

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,7 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The puspose of makeCacheMatrix is to store a martix and a cached value of the inverse of the matrix
+
+## While cacheSolve should computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 ## Write a short comment describing this function
 
